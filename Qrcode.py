17907ed512c8e8b9948e8344da7d07dd59ec84ab
Kim kosei <40720090+davidhuotkeo@users.noreply.github.com>
2019-06-18 09:44:42 +0700

    Update Qrcode.py

--- Qrcode.py
+++ Qrcode.py
@@ -1,8 +1,40 @@
-# to use qrcode library we need to install it first
-# pip install qrcode
+# IMPORT LIBRARIES
 import qrcode
+import six
 
-# create qrcode
+# CREATE QRCODE CLASS
+class QRcode(object):
+    def __init__(self, qr_object):
+        # QROBJECT
+        self.qr_object = qr_object
+        # EXTENSIONS
+        self.ext = ['png', 'bmp', 'jpeg', 'jpg']
+
+    # PUT DATA TO THE QRCODE FOR GENERATION
+    def input_data(self, data):
+        # IF IT IS STRING OR INTEGER MAKE IT ELSE WRONG DATATYPE
+        if isinstance(data, int) or isinstance(data, six.string_types):
+            self.qr_object.add_data(data)
+            self.qr_object.make(fit=True)
+        else:
+            print('Wrong data type only integer or string')
+
+    # GENERATE PICTURE
+    # FILENAME CAN USE WITH PATH
+    def generate(self, filename, extension, path=''):
+        # IF THE EXTENSION IS INCLUDED IN ARRAY OF EXTENSION
+        if extension in self.ext:
+            img = self.qr_object.make_image()
+            # SAVE IMAGE
+            if path == '':
+                img.save('{}.{}'.format(filename, extension))
+            else:
+                if path[-1] == '/':
+                    path = path[:-1]
+                    img.save('{}/{}.{}'.format(path, filename, extension))
+
+# EXAMPLE
+# CREATE QR_OBJECT
 qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -10,18 +42,6 @@ qr = qrcode.QRCode(
         border=1,
     )
 
-# The data that you want to store
-data = "Kim Kosei"
-
-# Add data
-qr.add_data(data)
-qr.make(fit=True)
-
-# Create an image from the QR Code instance
-img = qr.make_image()
-
-# Save it somewhere, change the extension as needed:
-img.save("image.png")
-# img.save("image.bmp")
-# img.save("image.jpeg")
-# img.save("image.jpg")
+q = QRcode(qr)
+q.input_data('David is awesome')
+q.generate('david', 'png')
